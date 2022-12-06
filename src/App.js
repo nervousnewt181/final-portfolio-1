71dd7ec76d478ad163e0532c8e2e5a8a4a733979
ejiang24 <ethan_jiang@brown.edu>
2022-12-06 18:40:30 -0500

    bruh

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   return (
     <div className="App">
       
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <Routes>
               <Route path="/" element={<Home />}/>
               <Route path="/kolahfarangi/*" element={<KolahFarangi />}/>  
