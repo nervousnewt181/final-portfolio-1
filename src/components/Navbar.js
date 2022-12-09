e2fe0ae1d74f4fbac895787ace3605691c7f815d
ejiang24 <ethan_jiang@brown.edu>
2022-12-08 19:06:32 -0500

    undoing shit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar() {
                 //     left: 0,
                 //     behavior: "instant",
                 // });
-                window.scrollTo(0, 830);
+                window.scrollTo(0, 730);
             }
             if (scrollTo === "about") {
                 window.scrollTo({
